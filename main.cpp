1403b2250955a79a003919b73f55de69d35d6dc8
dexter <dexter@MDexter>
2014-03-25 16:00:19 -0700

    first cleam app

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: dexter
+ *
+ * Created on March 25, 2014, 3:48 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
